25261085f22eafe74fb3689dc7faf053dddda2a3
Mahbub <mahbub.buet.cse18@gmail.com>
2025-07-05 20:55:20 +0600

    Day-1 with defer and async

---
+++ Day-01/Task/script.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.log("Day-01 from Days 40 of JavaScript");
+document.write("Message in Console")
+document.getElementById("id").innerText ="Hello from JS";
---
+++ Day-01/test.js
@@ -0,0 +1,5 @@
+function init(){
+  document.getElementById("id").innerText = "Some Text";
+}
+console.log("Hello")
+init();
